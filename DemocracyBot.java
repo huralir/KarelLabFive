3c03d57462ea1569b55fe5e1ba8c26d93095509c
huralir <31323095+huralir@users.noreply.github.com>
2017-09-26 12:24:25 -0700

    Update DemocracyBot.java

--- DemocracyBot.java
+++ DemocracyBot.java
@@ -1,4 +1,4 @@
-package KarelLabFive;
+ 
 
 
 import kareltherobot.*; 
@@ -9,13 +9,64 @@ import kareltherobot.*;
  */
 public class DemocracyBot extends Robot
 {
+    int x;
     public DemocracyBot(int st, int av, Direction dir, int numBeepers) {
         super(st, av, dir, numBeepers);
     }
-    
     public void fixBallots() {
-        
+        for (x = 0; x < 5; x++) {
+            move();
+            if (nextToABeeper()) {
+                fillRoom();
+            }
+            else {
+                emptyRoom();
+            }
+            move();
+        }
     }
-   
+    public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    public void turnAround() {
+        turnLeft();
+        turnLeft();
+    }
+    public void fillRoom() {
+        turnLeft();
+        move();
+        while (nextToABeeper()) {
+            pickBeeper();
+        }
+        putBeeper();
+        turnAround();
+        move();
+        move();
+        while (nextToABeeper()) {
+            pickBeeper();
+        }
+        putBeeper();
+        turnAround();
+        move();
+        turnRight();
+    }
+    public void emptyRoom() {
+        turnLeft();
+        move();
+        while (nextToABeeper()) {
+            pickBeeper();
+        }
+        turnAround();
+        move();
+        move();
+        while (nextToABeeper()) {
+            pickBeeper();
+        }
+        turnAround();
+        move();
+        turnRight();
+    }
 }
 
